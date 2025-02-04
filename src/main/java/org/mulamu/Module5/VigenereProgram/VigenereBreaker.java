749140181f2bc85d1ecc97b83c16f87354ae89dc
Mulamuleli Mammba <84395540+MulamuMammba@users.noreply.github.com>
2025-02-04 11:33:51 +0200

    Module 5 Second Graded Activity

--- src/main/java/org/mulamu/Module5/VigenereProgram/VigenereBreaker.java
+++ src/main/java/org/mulamu/Module5/VigenereProgram/VigenereBreaker.java
@@ -1,12 +1,14 @@
 package org.mulamu.Module5.VigenereProgram;
 
-import java.util.*;
-import edu.duke.*;
+import edu.duke.FileResource;
+
+import java.util.Arrays;
+import java.util.HashSet;
 
 public class VigenereBreaker {
     public String sliceString(String message, int whichSlice, int totalSlices) {
         StringBuilder result = new StringBuilder();
-        for (int i = whichSlice; i < message.length();i+=totalSlices) {
+        for (int i = whichSlice; i < message.length(); i += totalSlices) {
             result.append(message.charAt(i));
         }
         return result.toString();
@@ -27,22 +29,13 @@ public class VigenereBreaker {
 
 
     public void breakVigenere() {
-        FileResource fr = new FileResource("src/main/java/org/mulamu/Module5/VigenereProgram/messages/secretmessage1.txt");
+        FileResource fr = new FileResource("src/main/java/org/mulamu/Module5/VigenereProgram/messages/secretmessage2.txt");
+
         String encrypted = fr.asString();
 
-        char mostCommon = 'e';
+        FileResource dictionary = new FileResource("src/main/java/org/mulamu/Module5/VigenereProgram/dictionaries/English");
 
-        int klength = 4;
-        int[] key = tryKeyLength(encrypted, klength, mostCommon);
-
-        System.out.println("Keys used\n" +
-                Arrays.toString(key));
-
-
-        VigenereCipher vc = new VigenereCipher(key);
-        String decrypted = vc.decrypt(encrypted);
-
-        System.out.println("Decrypted message: " + decrypted);
+        breakForLanguage(encrypted,readDictionary(dictionary));
     }
 
 
@@ -54,5 +47,68 @@ public class VigenereBreaker {
         return dictionary;
     }
 
+    private int countWords(String message, HashSet<String> dictionary) {
+        int count = 0;
+        String[] words = message.split("\\W+");
+
+        for (String word : words) {
+            if (dictionary.contains(word.toLowerCase())) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    private void breakForLanguage(String encrypted, HashSet<String> dictionary) {
+        int maxIterations = 100;
+        char mostCommon = 'e';
+        int highestRealWords = 0;
+        int[] realKey = new int[0];
+        String possibleMessage = "";
+        int keyLenth = 0;
+
+        for (int i = 1; i < maxIterations; i++) {
+            int[] key = tryKeyLength(encrypted, i, mostCommon);
+            VigenereCipher cipher = new VigenereCipher(key);
+            String decrypted = cipher.decrypt(encrypted);
+            int realWords = countWords(decrypted, dictionary);
+            if (realWords > highestRealWords) {
+                highestRealWords = realWords;
+                realKey = key;
+                possibleMessage = decrypted;
+                keyLenth = i;
+
+            }
+        }
+
+
+//        int key_length = 38;
+//            int[] key = tryKeyLength(encrypted, key_length, mostCommon);
+//            VigenereCipher cipher = new VigenereCipher(key);
+//            String decrypted = cipher.decrypt(encrypted);
+//            int realWords = countWords(decrypted, dictionary);
+//            if (realWords > highestRealWords) {
+//                highestRealWords = realWords;
+//                realKey = key;
+//                possibleMessage = decrypted;
+//                keyLenth = key_length;
+//
+//            }
+
+
+        String alph = "abcdefghijklmnopqrstuvwxyz";
+        StringBuilder keyWord = new StringBuilder();
+
+        for (int letter: realKey){
+            keyWord.append(alph.charAt(letter));
+        }
+
+        System.out.println("Possible Message\n"
+                + "Key Length : " + keyLenth + "\n"
+                + "Key : " + Arrays.toString(realKey) + " or " + keyWord + "\n"
+                + "This message has " + highestRealWords + " real words\n"
+                + "Message : " + possibleMessage);
+    }
 
 }
